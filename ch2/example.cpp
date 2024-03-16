d424684a93d129190084e025ddbd90845ca6642b
PoBear1 <116469184+PoBear1@users.noreply.github.com>
2024-03-16 16:57:32 +1100

    Update example.cpp

--- ch2/example.cpp
+++ ch2/example.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <complex>
 int main() {
-	cline circle(1,1,0,0);
+	cline circle(2,0,0,-2);
 	matrix invert_mat{{{0,1},{1,0}}};
 	cline invert=circle.mobius(invert_mat);
 	std::cout<<"\"circle\" centre: "<<circle.centre()<<", radius: "<<circle.radius()<<std::endl;
@@ -11,6 +11,18 @@ int main() {
 	std::cout<<"\"invert\" parameters: ";
 	std::array<long double,4> a=invert.get_params();
 	for(auto i:a) {
+		std::cout<<i<<", ";
+	}
+	std::cout<<"\b\b  "<<std::endl;
+	cline line(0,2,0,2);
+	cline invert_line=line.mobius(invert_mat);
+	std::cout<<"\"line\" offset: "<<line.offset()<<", dir: "<<line.direction()<<std::endl;
+	std::cout<<"\"line\" centre: "<<line.centre()<<", radius: "<<line.radius()<<std::endl;
+	std::cout<<"\"invert_line\" centre: "<<invert_line.centre()<<", radius: "<<invert_line.radius()<<std::endl;
+	std::cout<<"\"invert_line\" offset: "<<invert_line.offset()<<", dir: "<<invert_line.direction()<<std::endl;
+	std::cout<<"\"invert_line\" parameters: ";
+	std::array<long double,4> b=invert_line.get_params();
+	for(auto i:b) {
 		std::cout<<i<<", ";
 	}
 	std::cout<<"\b\b  "<<std::endl;
