83758c8f90c9e08e16fc1c3802f95c898858b576
PoBear1 <116469184+PoBear1@users.noreply.github.com>
2024-03-17 11:27:56 +1100

    Update example.cpp

--- ch2/example.cpp
+++ ch2/example.cpp
@@ -1,8 +1,9 @@
 #include "../cmplx_lib/complex_calc.h"
+#include "../cmplx_lib/asy_drawing.h"
 #include <iostream>
 #include <complex>
 int main() {
-	cline circle(2,0,0,-2);
+	cline circle(2,2,0,0);
 	matrix invert_mat{{{0,1},{1,0}}};
 	cline invert=circle.mobius(invert_mat);
 	std::cout<<"\"circle\" centre: "<<circle.centre()<<", radius: "<<circle.radius()<<std::endl;
@@ -14,7 +15,7 @@ int main() {
 		std::cout<<i<<", ";
 	}
 	std::cout<<"\b\b  "<<std::endl;
-	cline line(0,2,0,2);
+	cline line(0,2,0,-2);
 	cline invert_line=line.mobius(invert_mat);
 	std::cout<<"\"line\" offset: "<<line.offset()<<", dir: "<<line.direction()<<std::endl;
 	std::cout<<"\"line\" centre: "<<line.centre()<<", radius: "<<line.radius()<<std::endl;
@@ -26,5 +27,11 @@ int main() {
 		std::cout<<i<<", ";
 	}
 	std::cout<<"\b\b  "<<std::endl;
+	draw_asy example("example.asy");
+	example.draw(circle,"heavygreen");
+	example.draw(invert,"orange");
+	example.draw(line,"orange");
+	example.draw(invert_line,"heavygreen");
+	example.close();
 	return 0;
 }
